9f799cdbe3dad13c93dc6465cb75c16af2f37037
root <altair.trabajo@gmail.com>
2017-04-22 19:42:53 +0000

    Proyecto final ASIX 2016-2017

---
+++ radius_server/Radius-help.md
@@ -0,0 +1,8 @@
+RADIUS HELP
+==========
+
+ASIX M06-ASO 2016-2017 @altaitisx
+---------------------------
+
+### Help descriptivo del acceso al servidor radius
+
---
+++ radius_server/startup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+/usr/bin/echo "Iniciar el servidor RADIUS"
+
+#/usr/bin/echo "starting sshd-keygen"
+#/usr/sbin/sshd-keygen && echo "ok"
+
+#/usr/bin/echo "starting sshd"
+#/usr/sbin/sshd -D $OPTIONS && echo "ok"
+#/usr/sbin/sshd && echo "ok"
+
+
