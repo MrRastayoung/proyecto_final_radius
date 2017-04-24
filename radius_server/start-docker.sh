6cad8a87977e87a3b6b9a26c3a72be84738678f3
altairisx <altair.trabajo@gmail.com>
2017-04-24 19:12:26 +0200

    create script start-docker.sh

---
+++ radius_server/start-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### Script que crea un container a partir de un Dockerfile.
+### El fin de este script es probar la creacion de containers a partir
+### del dockerfile, con el ir construyendolo e ir elaborando una serie
+### de comprobaciones automaticas para saber que todo funciona correctamente
+
+
+### Docker build apartir de un dockerfile
+
