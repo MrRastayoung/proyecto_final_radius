d501aaa72d8008a38b83009acd770dd1687a0f06
MrRastayoung <altair.trabajo@gmail.com>
2017-04-25 19:30:32 +0200

    Update start

--- radius_server/start-docker.sh
+++ radius_server/start-docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 ### Script que crea un container a partir de un Dockerfile.
-### El fin de este script es probar la creacion de containers a partir
-### del dockerfile, con el ir construyendolo e ir elaborando una serie
+### El fin de este script es probar la creacion de imagenes a partir
+### de un dockerfile.
+### Con el, ir construyendo y elaborando una serie
 ### de comprobaciones automaticas para saber que todo funciona correctamente
 
 
 ### Docker build apartir de un dockerfile
 docker build -t proyecto_final/radius .
-
-### Arrancar container con tiempo de vida limitado
-docker run --rm --name radius -id proyecto_final/radius
