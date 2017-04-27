fdc38ef554f0fbc454f1f7d4944822c4c0b6a188
root <altair.trabajo@gmail.com>
2017-04-27 09:02:51 +0000

    modified Dockerfile Radius and Mysql

--- mysql_server/start-container.sh
+++ mysql_server/start-container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Nombre que queremos que tenga el container
-CONTAINER_NAME=radius
+CONTAINER_NAME=mysql
 
 # Imagen que utilizaremos ( imagen creada con docker file )
-REPOSITORI_IMAGE="proyecto_final/radius"
+REPOSITORI_IMAGE="proyecto_final/mysql"
 
 ### Arrancar container con tiempo de vida limitado
 docker run --rm --name $CONTAINER_NAME -id $REPOSITORI_IMAGE /bin/bash
